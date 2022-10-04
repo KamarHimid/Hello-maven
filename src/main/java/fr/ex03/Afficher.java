82ea061f93fbd7fd3026d2581bbf4af7f87dc82a
Kamar <kamar.himid@gmail.com>
2022-10-04 10:58:57 +0200

    V2 avec classes

---
+++ src/main/java/fr/ex03/Afficher.java
@@ -0,0 +1,9 @@
+package fr.ex03;
+
+public class Afficher{
+	
+	public void display(String message)
+    {
+    System.out.println(message) ;
+    }
+}
--- src/main/java/fr/ex03/HelloWorld.java
+++ src/main/java/fr/ex03/HelloWorld.java
@@ -6,6 +6,9 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		
 		//Afficher le message
+		String message="Hello Maven World";
+		Afficher A = new Afficher();
+		A.display(message);
 		System.out.println("Hello Maven");
 
 	}
