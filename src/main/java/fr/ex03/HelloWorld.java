76106580182d62e30d8040ef54686627b1471db5
Kamar <kamar.himid@gmail.com>
2022-10-04 11:17:16 +0200

    v4 bon commentaire

--- src/main/java/fr/ex03/HelloWorld.java
+++ src/main/java/fr/ex03/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Afficher le message
+		//Afficher le message en clair
 		String message="Hello Maven World";
 		Afficher A = new Afficher();
 		A.display(message);
